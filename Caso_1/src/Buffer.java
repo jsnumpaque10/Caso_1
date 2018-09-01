d8d6b3bda6438a4f0af94982f96ed81819f30c71
js.numpaque10 <js.numpaque10@ISC235.sis.virtual.uniandes.edu.co>
2018-09-01 11:45:51 -0500

    Más modificaciones Buffer

--- Caso_1/src/Buffer.java
+++ Caso_1/src/Buffer.java
@@ -5,21 +5,53 @@ public class Buffer
 	 * Variable que modela la cantidad de clientes del sistema en un tiempo dado.
 	 */
 	private int numClientes; 
+	
 	/**
 	 * Variable que modela la cantidad limitada de mensajes que puede almacenar el buffer.
 	 */
 	private int capacidadMensajes;
+	
 	/**
 	 * Variable que modela la cantidad de mensajes que hay en el buffer en un momento dado.
 	 */
-	private int cantidadMensajes; 
+	private int cantidadMensajes;
 	
-	public Buffer(int pCapacidadMensajes, int pNumClientes)
+	/**
+	 * Contenedor de todos los mensajes que recibe el buffer.
+	 */
+	private Mensaje[] Mensajes;
+	
+	public Buffer(int pCapacidadMensajes)
 	{
 		setCapacidadMensajes(pCapacidadMensajes);
-		setNumClientes(pNumClientes);
+		setNumClientes(0);
 		setCantidadMensajes(0);
+		Mensajes = new Mensaje[pCapacidadMensajes];
 	}
+	
+	/**
+	 * Método que ingresa un cliente al buffer para que pueda enviar mensajes.
+	 */
+	public void ingresarCliente()
+	{
+		synchronized(this){
+			numClientes++;
+		}
+		
+	}
+	
+	/**
+	 * Método que retira un cliente del buffer cuando y ha enviado todos sus mensajes.
+	 */
+	
+	public void retirarCliente()
+	{
+		synchronized(this){
+			numClientes--;
+		}
+	}
+	
+	
 
 	public int getNumClientes() {
 		return numClientes;
